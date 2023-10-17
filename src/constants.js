8f36af95ce15a605af826718ceb49ed9fe20c702
Katie <katie.oh@outlook.com>
2023-10-17 16:36:34 -0400

    Format

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const { Server } = require('socket.io');
-const io = require('socket.io-client');
-const http = require('http');
+const express = require("express");
+const { Server } = require("socket.io");
+const io = require("socket.io-client");
+const http = require("http");
 
 const PORT = 2468;
-const URL = 'https://online-go.com'; // OGS URL
-const PARAMS = { "transports": ["websocket"] }; // OGS PARAMS
+const URL = "https://online-go.com"; // OGS URL
+const PARAMS = { transports: ["websocket"] }; // OGS PARAMS
 
 const APP = express();
 const HTTP_SERVER = http.createServer(APP);
 const BES = new Server(HTTP_SERVER); // Using Server constructor backend server
-const OGS = io(URL, PARAMS); // OGS connection using 'io' 
+const OGS = io(URL, PARAMS); // OGS connection using 'io'
 const GAMES = {};
 
-const isLinux = process.env.IS_LINUX
-const AIEXE = isLinux ? "./katago-linux/katago" : "./katago/katago.exe"
-const AICONFIG = isLinux ? "./katago-linux/default_config.cfg" : "./katago/default_config.cfg"
-const AIMODEL = isLinux ? "./katago-linux/default_model.bin.gz" : "./katago/default_model.bin.gz"
+const isLinux = process.env.IS_LINUX;
+const AIEXE = isLinux ? "./katago-linux/katago" : "./katago/katago.exe";
+const AICONFIG = isLinux ? "./katago-linux/default_config.cfg" : "./katago/default_config.cfg";
+const AIMODEL = isLinux ? "./katago-linux/default_model.bin.gz" : "./katago/default_model.bin.gz";
 
-module.exports = {APP, AIEXE, AICONFIG, AIMODEL, BES, GAMES,HTTP_SERVER, OGS, PORT}
+module.exports = { APP, AIEXE, AICONFIG, AIMODEL, BES, GAMES, HTTP_SERVER, OGS, PORT };
