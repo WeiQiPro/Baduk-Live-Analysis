9ddf8943d49ed82e889b0f0b85da4b9af0219042
Jeremiah  Donley <jaeldonley@gmail.com>
2025-07-09 06:19:24 -0400

    removal

--- BLA-Archived/web/js/scorebar.js
+++
@@ -1,6 +0,0 @@
-import { scoreBarRenderer } from "./scoreBarRenderer.js";
-
-export function setScoreBar(blackScores, whiteScores) {
-    console.log({ blackScores, whiteScores });
-    scoreBarRenderer.updateScoreBar(blackScores, whiteScores);
-}
